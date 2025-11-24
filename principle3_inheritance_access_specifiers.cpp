4a3afce429905980307e506e373846f87f4d29eb
Zakaria Adriouche <zakariaadriouche@gmail.com>
2025-11-24 13:01:11 +0100

    Access modifiers (or access specifiers) are keywords in object-oriented languages that set the accessibility of classes, methods, and other members.

---
+++ principle3_inheritance_access_specifiers.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+class clsA
+{
+    private:
+    int _Var1 = 1;
+    void _Func(void)
+    {
+        std::cout << "Function 1\n";
+    }
+
+    protected:
+    int Var2 = 2;
+    void Func2(void)
+    {
+        std::cout << "Function 2\n";
+    }
+
+    public:
+    int Var3 = 3;
+    void Func3(void)
+    {
+        std::cout << "Function 3\n";
+    }
+};
+
+class clsB : public clsA
+{
+    public:
+    void Func4(void)
+    {
+        Func2();
+        clsA::Func2();
+        clsA::Func3();
+    }
+
+    void Func5(void)
+    {
+        std::cout << Var2 << std::endl;
+        std::cout << clsA::Var2 << std::endl;
+        std::cout << clsA::Var3 << std::endl;
+        std::cout << Var3 << std::endl;
+    }
+};
+
+int main(void)
+{
+    clsB B;
+
+    B.Func3();
+    B.Func4();
+    B.Func5();
+
+    B.Var3 = 42;
+    std::cout << B.Var3 << std::endl;
+
+    return (0);
+}
